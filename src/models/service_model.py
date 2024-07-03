349477f20d183a61848a854028cc09b78c015a53
aron tarnoy <tarnoyaa@gmail.com>
2024-07-03 14:29:01 +0200

    update service model with extras

--- src/models/service_model.py
+++ src/models/service_model.py
@@ -1,6 +1,12 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Enum
 from sqlalchemy.orm import Session
 from .base import BaseModel
+import enum
+
+class ServiceType(enum.Enum):
+    PACKAGE = "package"
+    EXTRA_OUTSIDE = "extra_outside"
+    EXTRA_INSIDE = "extra_inside"
 
 class ServiceModel(BaseModel):
     __tablename__ = 'Service'
@@ -9,16 +15,18 @@ class ServiceModel(BaseModel):
     service_name = Column(String, nullable=False)
     price_small = Column(Integer, nullable=False)
     price_large = Column(Integer, nullable=False)
+    service_type = Column(Enum(ServiceType), nullable=False)
 
     def __repr__(self):
-        return f"<ServiceModel(id={self.id}, service_name='{self.service_name}', price={self.price})>"
+        return f"<ServiceModel(id={self.id}, service_name='{self.service_name}', service_type={self.service_type})>"
 
     @classmethod
-    def add_service(cls, session: Session, service_name: str, price_small: int, price_large: int):
+    def add_service(cls, session: Session, service_name: str, price_small: int, price_large: int, service_type: ServiceType):
         service = cls(
             service_name = service_name,
             price_small = price_small,
-            price_large = price_large
+            price_large = price_large,
+            service_type = service_type
         )
         session.add(service)
         session.commit()
