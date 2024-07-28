2a60729d72f6eaa0c300de5b116473977f69b8b5
danitarnoy <danitarnoy@gmail.com>
2024-07-28 05:24:13 -0700

    override reservation create method for admin

--- src/models/car_model.py
+++ src/models/car_model.py
@@ -1,17 +1,19 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Enum as SqlEnum
 from sqlalchemy.orm import Session
 from .base import BaseModel
-from sqlalchemy import Enum
+from enum import Enum as PyEnum
 from sqlalchemy.orm import relationship
 
-CarTypeEnum = Enum('small_car', 'large_car', name='car_type_enum')
+class CarTypeEnum(PyEnum):
+    small_car = "small_car"
+    large_car = "large_car"
 
 class CarModel(BaseModel):
     __tablename__ = 'Car'
     
     id = Column(Integer, primary_key=True)
     license_plate = Column(String, nullable=False)
-    car_type = Column(CarTypeEnum, nullable=False)
+    car_type = Column(SqlEnum(CarTypeEnum), nullable=False)
     car_brand = Column(String,nullable=False)
     company_id = Column(Integer, ForeignKey('Company.id'), nullable=True)
 
