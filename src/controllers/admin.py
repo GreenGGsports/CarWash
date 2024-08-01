5a9b0057b0d20fabc86bdb08bf29864586e3b91a
danitarnoy <danitarnoy@gmail.com>
2024-08-01 10:44:47 -0700

    admin manage relationships on reservation form

--- src/controllers/admin.py
+++ src/controllers/admin.py
@@ -9,7 +9,6 @@ from src.models.slot_model import SlotModel
 from src.models.user_model import UserModel
 from src.models.carwash_model import CarWashModel
 from src.models.extra_model import ExtraModel
-from src.models.reservation_extras import reservation_extra
 from flask_admin.contrib.sqla import ModelView
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, IntegerField, FloatField, DateTimeField
@@ -17,7 +16,7 @@ from wtforms_sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
 
 class ServiceModelView(ModelView):
     column_labels = {
-        'carwash.carwash_name': 'Car Wash Name',  # Itt adod meg az egyedi nevet
+        'carwash.carwash_name': 'Car Wash Name',
         'service_name': 'Service Name',
         'price_small': 'Price (Small)',
         'price_large': 'Price (Large)'
@@ -36,6 +35,7 @@ class ReservationForm(FlaskForm):
         self.slot.query_factory = lambda: self.session.query(SlotModel).all()
         self.extras.query_factory = lambda: self.session.query(ExtraModel).all()
 
+
     new_car_license_plate = StringField('New Car License Plate')
     new_car_type = SelectField('New Car Type', choices=[(t.name, t.name) for t in CarTypeEnum])
     new_car_brand = StringField('New Car Brand')
@@ -49,18 +49,15 @@ class ReservationForm(FlaskForm):
     service = QuerySelectField('Service', allow_blank=False, query_factory=lambda: [])
     extras = QuerySelectMultipleField('Extras', get_label='service_name')
 
-
     reservation_date = DateTimeField('Reservation Date', format='%Y-%m-%d %H:%M:%S')
     parking_spot = IntegerField('Parking Spot')
     final_price = FloatField('Final Price')
     carwash = QuerySelectField('Carwash', allow_blank=False, query_factory=lambda: [])
     slot = QuerySelectField('Slot', allow_blank=False, query_factory=lambda: [])
 
+
 class ReservationAdminView(ModelView):
     form = ReservationForm
-    # edit_template = 'admin/reservation_admin.html'
-    # create_template = 'admin/reservation_admin.html'
-
     column_list = (
         'reservation_date',
         'car.license_plate',
@@ -88,71 +85,99 @@ class ReservationAdminView(ModelView):
 
     form_excluded_columns = ['billing']
 
-    def __init__(self, model, session_factory, *args, **kwargs):
-        self.session_factory = session_factory
-        super(ReservationAdminView, self).__init__(model, session_factory.get_session(), *args, **kwargs)
+    def __init__(self, model, session, *args, **kwargs):
+        self.session = session
+        super(ReservationAdminView, self).__init__(model, session, *args, **kwargs)
 
     def create_form(self, obj=None):
         form = super(ReservationAdminView, self).create_form(obj)
-        form.session = self.session_factory.get_session()
+        form.session = self.session
         return form
 
     def edit_form(self, obj=None):
         form = super(ReservationAdminView, self).edit_form(obj)
-        form.session = self.session_factory.get_session()
+        form.session = self.session
         return form
 
     def on_model_change(self, form, model, is_created):
-        session = self.session_factory.get_session()
+        session = form.session
+
         
-        # Handle new customer
-        if form.new_customer_forname.data and form.new_customer_lastname.data and form.new_customer_phone_number.data:
-            customer = session.query(CustomerModel).filter_by(
-                forname=form.new_customer_forname.data,
-                lastname=form.new_customer_lastname.data,
-                phone_number=form.new_customer_phone_number.data
-            ).first()
-            if not customer:
-                customer = CustomerModel(
-                    forname=form.new_customer_forname.data,
-                    lastname=form.new_customer_lastname.data,
-                    phone_number=form.new_customer_phone_number.data
-                )
-                session.add(customer)
-                session.commit()
-            model.customer_id = customer.id
+        try:
+            # Use no_autoflush to prevent automatic flushes
+            with session.no_autoflush:
+                # Ensure that all related models are attached to the session
+                if form.service.data:
+                    model.service = session.merge(ServiceModel(id=form.service.data.id))
+                if form.carwash.data:
+                    model.carwash = session.merge(CarWashModel(id=form.carwash.data.id))
+                if form.slot.data:
+                    model.slot = session.merge(SlotModel(id=form.slot.data.id))
+                if form.extras.data:
+                    model.extras = [session.merge(ExtraModel(id=extra.id)) for extra in form.extras.data]
 
-        # Handle new car
-        if form.new_car_license_plate.data and form.new_car_type.data and form.new_car_brand.data:
-            car = session.query(CarModel).filter_by(
-                license_plate=form.new_car_license_plate.data,
-                car_type=CarTypeEnum[form.new_car_type.data],
-                car_brand=form.new_car_brand.data
-            ).first()
-            if not car:
-                car = CarModel(
-                    license_plate=form.new_car_license_plate.data,
-                    car_type=CarTypeEnum[form.new_car_type.data],
-                    car_brand=form.new_car_brand.data
-                )
-                session.add(car)
-                session.commit()
-            model.car_id = car.id
+                # Handle new customer
+                if form.new_customer_forname.data and form.new_customer_lastname.data and form.new_customer_phone_number.data:
+                    customer = session.query(CustomerModel).filter_by(
+                        forname=form.new_customer_forname.data,
+                        lastname=form.new_customer_lastname.data,
+                        phone_number=form.new_customer_phone_number.data
+                    ).first()
+                    if not customer:
+                        customer = CustomerModel(
+                            forname=form.new_customer_forname.data,
+                            lastname=form.new_customer_lastname.data,
+                            phone_number=form.new_customer_phone_number.data
+                        )
+                        session.add(customer)
+                        session.commit()  # Commit to get the customer_id
+                    model.customer_id = customer.id
+                    model.customer = customer
 
-        session.close()  # Close the session
+                # Handle new car
+                if form.new_car_license_plate.data and form.new_car_type.data and form.new_car_brand.data:
+                    car = session.query(CarModel).filter_by(
+                        license_plate=form.new_car_license_plate.data,
+                        car_type=CarTypeEnum[form.new_car_type.data],
+                        car_brand=form.new_car_brand.data
+                    ).first()
+                    if not car:
+                        car = CarModel(
+                            license_plate=form.new_car_license_plate.data,
+                            car_type=CarTypeEnum[form.new_car_type.data],
+                            car_brand=form.new_car_brand.data
+                        )
+                        session.add(car)
+                        session.commit()
+                    model.car_id = car.id
+                    model.car = car
+
+                # Flush changes manually
+                session.flush()
+
+        except Exception as e:
+            print(f"Exception occurred: {e}")
+            raise
 
         return super(ReservationAdminView, self).on_model_change(form, model, is_created)
-    
+
+
+
 def init_admin(app, session_factory):
     admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
 
+    session = session_factory.get_session()
+
     # Admin view registration
-    admin.add_view(ReservationAdminView(ReservationModel, session_factory))
-    # admin.add_view(ModelView(UserModel, app.session_factory.get_session()))
-    # admin.add_view(ModelView(SlotModel, app.session_factory.get_session()))
-    # admin.add_view(ServiceModelView(ServiceModel, app.session_factory.get_session()))
-    # admin.add_view(ModelView(CompanyModel, app.session_factory.get_session()))
-    # admin.add_view(ModelView(CarWashModel, app.session_factory.get_session()))
-    # admin.add_view(ModelView(ExtraModel, app.session_factory.get_session()))
-    # admin.add_view(ModelView(CustomerModel, app.session_factory.get_session()))
-    # admin.add_view(ModelView(BillingModel, app.session_factory.get_session()))
+    admin.add_view(ReservationAdminView(ReservationModel, session))
+    admin.add_view(ModelView(UserModel, session))
+    admin.add_view(ModelView(SlotModel, session))
+    admin.add_view(ServiceModelView(ServiceModel, session))
+    admin.add_view(ModelView(CompanyModel, session))
+    admin.add_view(ModelView(CarWashModel, session))
+    admin.add_view(ModelView(ExtraModel, session))
+    admin.add_view(ModelView(CustomerModel, session))
+    admin.add_view(ModelView(BillingModel, session))
+    admin.add_view(ModelView(CarModel, session))
+
+    session.close()
--- src/views/reservation_view.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, request
-import os 
-
-reservation_view = Blueprint('reservation_view', __name__, template_folder='templates')
-
-class ReservationView:  
-    @staticmethod
-    def show_reservation_form():
-        return render_template('reservation.html')
