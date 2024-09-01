fdb45f50e005d39406d750f6b658f4d02057ee7a
danitarnoy <danitarnoy@gmail.com>
2024-09-01 09:57:52 -0700

    .

--- src/views/my_modelview.py
+++ src/views/my_modelview.py
@@ -3,6 +3,7 @@ from flask import flash , current_app
 
 class MyModelView(ModelView):
     def on_model_change(self, form, model, is_created):
+        self.session = current_app.session_factory.get_session()
         try:
             self.session.add(model)
             self.session.commit()
@@ -12,6 +13,7 @@ class MyModelView(ModelView):
 
         # Call the parent class's on_model_change method
         super(MyModelView, self).on_model_change(form, model, is_created)
+        self.session.close()
 
     def handle_view_exception(self, exc):
         flash(f'An unexpected error occurred: {exc}', 'error')
