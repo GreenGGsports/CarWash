3d20bbed4b16cb1f20f36bba8bd4f8eb2d170d9d
aron tarnoy <tarnoyaa@gmail.com>
2024-09-04 14:48:48 +0200

    remove Corse

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 
 #app = create_app('deployment')
 app = create_app('deployment')
-CORS(app)
 #app.run(debug=True)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
