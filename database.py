a97d89b429993572d8392f72283b30d139b42d23
aron tarnoy <tarnoyaa@gmail.com>
2024-09-19 17:37:55 +0200

    .

--- database.py
+++ database.py
@@ -45,8 +45,8 @@ def connect_tcp_socket() -> sqlalchemy.engine.base.Engine:
             port=db_port,
             database=db_name
         ),
-        pool_size=1,
-        max_overflow=0,
+        pool_size=5,
+        max_overflow=10,
         pool_timeout=30,
         pool_recycle=600,  # Recycle connections every 10 minutes
         pool_pre_ping=True,  # Ping connections before using them
