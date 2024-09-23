965f7a74e99ae25fc951f793387b5f514192c78b
danitarnoy <danitarnoy@gmail.com>
2024-09-23 19:06:42 +0200

    licence plate can only be upper case caracters

--- static/admin/scripts/scripts.js
+++ static/admin/scripts/scripts.js
@@ -1,18 +1,24 @@
 // script.js
+
 $(document).ready(function() {
+    // Automatically convert input to uppercase while typing
+    $('#new_car_license_plate').on('input', function() {
+        $(this).val($(this).val().toUpperCase());
+    });
+
     $('#new_car_license_plate').autocomplete({
         source: function(request, response) {
             $.ajax({
                 url: '/api/car/get_car_data',
                 dataType: 'json',
                 data: {
-                    license_plate: request.term
+                    license_plate: request.term.toUpperCase() // Convert input to uppercase before sending
                 },
                 success: function(data) {
                     if (data.error) {
                         response([]);
                     } else {
-                        // Kitöltjük a form mezőit az autocomplete adatokkal
+                        // Populate form fields
                         $('#new_car_license_plate').val(data.new_car_license_plate);
                         $('#new_car_type').val(data.new_car_type);
                         $('#new_car_brand').val(data.new_car_brand);
