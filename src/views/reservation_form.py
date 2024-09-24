fd1b5142bd236fa5310ed4fdc3407372258787b6
danitarnoy <danitarnoy@gmail.com>
2024-09-24 16:28:09 +0200

    reservation admin get attributes on edit

--- src/views/reservation_form.py
+++ src/views/reservation_form.py
@@ -10,7 +10,7 @@ from wtforms.validators import DataRequired
 from flask_login import current_user
 
 class ReservationForm(FlaskForm):
-    def __init__(self, session, *args, **kwargs):
+    def __init__(self, session, obj=None, *args, **kwargs):
         super(ReservationForm, self).__init__(*args, **kwargs)
         self.session = session
 
@@ -20,13 +20,46 @@ class ReservationForm(FlaskForm):
             self.carwash.query_factory = lambda: self.session.query(CarWashModel).filter_by(id=current_user.carwash.id).all()
             self.service.query_factory = lambda: self.session.query(ServiceModel).filter_by(carwash_id=current_user.carwash.id).all()
             self.extras.query_factory = lambda: self.session.query(ExtraModel).filter_by(carwash_id=current_user.carwash.id).all()
+            self.slot.query_factory = lambda: self.session.query(SlotModel).filter_by(carwash_id=current_user.carwash.id, live=True).all()
         else:
             self.carwash.query_factory = lambda: self.session.query(CarWashModel).all()
             self.service.query_factory = lambda: self.session.query(ServiceModel).all()
             
             self.extras.query_factory = lambda: self.session.query(ExtraModel).all()
 
-        self.slot.query_factory = lambda: []
+            self.slot.query_factory = lambda: self.session.query(SlotModel).filter_by(live=True).all()
+
+        if obj:
+            self.load_data(obj)
+
+    def load_data(self, obj):
+        """Betölti az adatokat a foglalás objektumból az űrlap mezőibe."""
+        if obj.car:
+            self.new_car_license_plate.data = obj.car.license_plate
+            self.new_car_type.data = obj.car.car_type.name  # Enum név
+            self.new_car_brand.data = obj.car.car_brand
+            self.new_car_model.data = obj.car.car_model
+
+        if obj.customer:
+            self.new_customer_forname.data = obj.customer.forname
+            self.new_customer_lastname.data = obj.customer.lastname
+            self.new_customer_phone_number.data = obj.customer.phone_number
+
+        self.reservation_date.data = obj.reservation_date
+        self.is_completed.data = obj.is_completed
+        self.billing_required.data = False
+
+        if obj.billing:
+            self.billing_required.data = True
+            self.billing_name.data = obj.billing.name
+            self.address.data = obj.billing.address
+            self.email.data = obj.billing.email
+            self.company_name.data = obj.billing.company_name
+            self.tax_ID.data = obj.billing.tax_ID
+
+        self.slot.data = obj.slot
+        self.service.data = obj.service  # A service objektumot állítjuk be
+        self.extras.data = obj.extras  # Az extras listát állítjuk be
         
     # Form fields
     new_car_license_plate = StringField('Rendszám', validators=[DataRequired()])
