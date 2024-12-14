c699ae9a3c5115c7e9c05cf5a7b225a99077c4d4
aron tarnoy <tarnoyaa@gmail.com>
2024-12-14 13:32:28 +0100

    design changes

--- src/views/customer_reservation.py
+++ src/views/customer_reservation.py
@@ -7,7 +7,7 @@ from src.models.carwash_model import CarWashModel
 from src.models.service_model import ServiceModel
 from src.models.extra_model import ExtraModel
 from src.models.customer_model import CustomerModel
-from flask import Flask, render_template, request, Blueprint, current_app
+from flask import Flask, render_template, request, Blueprint, current_app, session
 
 reservation_test = Blueprint('reservation_test', __name__, template_folder='templates')
 
@@ -15,23 +15,31 @@ reservation_test = Blueprint('reservation_test', __name__, template_folder='temp
 
 @reservation_test.route("/reserve")
 def show():
-    session = current_app.session_factory.get_session()
-    locations = session.query(CarWashModel).all()
-    services = session.query(ServiceModel).filter_by(carwash_id=1).all()
-    extras = session.query(ExtraModel).filter_by(carwash_id=1).all()
-    customer = session.query(CustomerModel).filter_by(user_id=1).first()
+    db_session = current_app.session_factory.get_session()
+    locations = db_session.query(CarWashModel).all()
+    services = db_session.query(ServiceModel).filter_by(carwash_id=1).all()
+    extras = db_session.query(ExtraModel).filter_by(carwash_id=1).all()
+    customer = db_session.query(CustomerModel).filter_by(user_id=1).first()
     return render(locations, services,extras=extras,customer=customer)
 
 @reservation_test.route("/select_location", methods=["GET", "POST"])
 def select_carwash():
     carwash_id = int(request.form.get("location_id"))
-    session = current_app.session_factory.get_session()
-    locations = session.query(CarWashModel).all()
-    services = session.query(ServiceModel).filter_by(carwash_id=carwash_id).all()
-    extras =  session.query(ExtraModel).filter_by(carwash_id=carwash_id).all()
-    return render(locations, services, extras)
+    session['carwash_id'] = carwash_id
+    db_session = current_app.session_factory.get_session()
+    locations = db_session.query(CarWashModel).all()
+    services = db_session.query(ServiceModel).filter_by(carwash_id=carwash_id).all()
+    extras =  db_session.query(ExtraModel).filter_by(carwash_id=carwash_id).all()
+    customer = db_session.query(CustomerModel).filter_by(user_id=1).first()
+    return render(locations, services, extras, customer, carwash_id)
 
-def render(locations, services, extras, customer):
-    return render_template('FoglalasV2.html', locations=locations, services=services, extras=extras, customer=customer)
+def render(locations, services, extras, customer, selected_location_id=None):
+    return render_template('FoglalasV2.html', locations=locations, services=services, extras=extras, customer=customer, selected_location_id=selected_location_id)
 
 
+
+@reservation_test.route("/create_reservation",methods=[ "POST"])
+def create_reservation():
+    from pdb import set_trace
+    set_trace()
+    pass
--- static/cust_script.js
+++ static/cust_script.js
@@ -60,22 +60,93 @@ document.getElementById('dateInput').addEventListener('change', (event) => {
     sendDateToServer(event.target.value);
 });
 function generateHourlyButtons(response) {
-    const template = document.getElementById("idopontKeret")
+    const template = document.getElementById("GombBox");
     const buttonContainer = document.querySelector(".GombBox");
+    buttonContainer.innerHTML = ""
 
     const slots = response['slots'];
     console.log(slots);
 
-    // Get the template content;
-    console.log(template)
     slots.forEach(slot => {
-        // Clone the template content
+        // Create a wrapper for the radio button and label
+        // Create the radio button
+        const radioButton = document.createElement('input');
+        radioButton.type = 'radio';
+        radioButton.name = 'timeSlot'; // Ensures only one can be selected
+        radioButton.classList.add(`radio`);
+        radioButton.classList.add(slot['available'])
+        radioButton.value = slot['id'];
+        radioButton.id = `slot-${slot['id']}`
+        radioButton.dataset.free = slot['available'];
 
-        // Customize the button content (e.g., using slot data)
-        const button = document.createElement('button');
-        button.classList.add('IdopontGomb', `free-${slot['available']}`);
-        button.innerText = `${slot['end_time_hours']}:${slot['end_time_minutes'].toString().padStart(2, '0')}`;
-        button.dataset.free = slot['available']
-        buttonContainer.appendChild(button);
+        // Create the label for the radio button
+        const label = document.createElement('label');
+        label.setAttribute("for", `slot-${slot['id']}`);
+        label.classList.add('IdopontLabel');
+        label.classList.add(slot['available'])
+        label.innerText = `${slot['end_time_hours']}:${slot['end_time_minutes'].toString().padStart(2, '0')}`;
+        // Append the radio button and label to the wrapper
+        buttonContainer.appendChild(radioButton);
+        buttonContainer.appendChild(label);
+
+        // Append the wrapper to the container
     });
+}
+
+const checkbox = document.getElementById("billing-required");
+// Checkbox eseménykezelő
+checkbox.addEventListener('change', () => {
+    const extraFields = document.getElementById("billing_data");
+    if (checkbox.checked) {
+        extraFields.classList.remove('hidden'); // Mezők megjelenítése
+    } else {
+        extraFields.classList.add('hidden'); // Mezők elrejtése
+    }
+});
+
+const paymentmethod = document.getElementById("payment_method")
+paymentmethod.addEventListener('change', () => {
+    const extraFields = document.getElementById("billing_data");
+    if (paymentmethod.value === "bankcard") {
+        extraFields.classList.remove('hidden'); // Mezők megjelenítése
+    } else {
+        extraFields.classList.add('hidden'); // Mezők elrejtése
+    }
+});
+
+function handle_errors(form){
+    var isvalid = true
+    var formData = new FormData(form);
+    for (var pair of formData.entries()) {
+        console.log(pair[0] + ": " + pair[1]);
+      }
+
+    if (formData.has("service")) {
+        console.log("Service is set.");
+        document.getElementById("serviceBox").classList.remove("error");
+    } else {
+        isvalid = false
+        document.getElementById("serviceBox").classList.add("error");
+    }
+    if (formData.has("timeSlot")) {
+        console.log("Slot is set.");
+        document.getElementById("GombBox").classList.remove("error");
+    }
+    else
+        isvalid = false
+        document.getElementById("GombBox").classList.add("error");
+    return isvalid
+}
+async  function create_reservation()
+{
+    form = document.getElementById("reservationForm")
+    if (!handle_errors(form)){
+        console.log("form is not valid")
+        return
+    }
+    if (form.checkValidity()) {
+        form.submit();  // Submit the form if valid
+      } else {
+        form.reportValidity(); // This triggers the browser's built-in validation messages
+    }
 }
