1b68d00966fd03f5c7033a32119d9c382e14d5b0
danitarnoy <danitarnoy@gmail.com>
2025-01-08 20:00:32 +0100

    secu updates

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ app = create_app('deployment')
 #app.run(debug=True)
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8000)
+    serve(app, host="127.0.0.1", port=8000)
     
